b12066c70521c11274d630c2286bfce0fbdb7663
Illya1123 <21520565@gm.uit.edu.vn>
2024-09-28 09:08:38 +0700

    sửa lỗi không hiển thị lines và points, bổ sung thêm code cho file map.js

--- map.js
+++ map.js
@@ -5,10 +5,11 @@ require([
   "esri/layers/GraphicsLayer",
   "esri/geometry/Point",
   "esri/geometry/Polygon",
+    "esri/geometry/Polyline",
   "esri/symbols/SimpleMarkerSymbol",
   "esri/symbols/SimpleFillSymbol",
   "dojo/domReady!"
-], function (Map, MapView, Graphic, GraphicsLayer, Point, Polygon, SimpleMarkerSymbol, SimpleFillSymbol) {
+], function (Map, MapView, Graphic, GraphicsLayer, Point, Polygon, Polyline, SimpleMarkerSymbol, SimpleFillSymbol, PicTureMarkerSymbol) {
   var map = new Map({
       basemap: "satellite",
   });
@@ -44,7 +45,33 @@ require([
               });
               graphicsLayer.add(polygonGraphic);
           });
-      })
+        jsondata.points.forEach(function (data) {
+            var pointGraphic = new Graphic({
+                geometry: new Point({
+                    x: data.longitude,
+                    y: data.latitude,
+                    spatialReference: { wkid: 4326 }
+                }),
+                symbol: data.symbol,
+                attributes: data,
+                popupTemplate: data.popupTemplate,
+                });
+                graphicsLayer.add(pointGraphic);
+            });
+        jsondata.lines.forEach(function (data) {
+            var lineGraphic = new Graphic({
+                geometry: new Polyline({
+                    paths: data.paths,
+                    spatialReference: { wkid: 4326 }
+                }),
+                symbol: data.symbol,
+                attributes: data,
+                popupTemplate: data.popupTemplate,
+            });
+            graphicsLayer.add(lineGraphic);
+            });
+        }
+    )
       .catch((error) => {
           console.error("Error fetching data:", error);
       });  
